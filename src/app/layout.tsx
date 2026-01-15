6d24bd586ea8905d25e5f2a61b2a1c8f253041bb
arfat.xponent <arfat.xponent@gmail.com>
2026-01-15 11:12:59 +0600

    minor changes included

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ServiceWorkerRegister from "@/components/ServiceWorkerRegistrar";
 import GlobalOnlineStatus from "@/components/GlobalOnlineStatus";
+import { WebVitals } from "@/components/web-vitals";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <WebVitals />
         <ServiceWorkerRegister />
         <GlobalOnlineStatus />
         {children}
